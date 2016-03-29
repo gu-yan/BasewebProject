09aac084280a66d5edfb7d3606ad8cf26088f213
julius_guyan <gu_yan@outlook.com>
2016-03-29 22:10:07 +0800

    controller

---
+++ springbase/web/src/main/java/cn/gy/test/web/controller/LoginController.java
@@ -0,0 +1,28 @@
+package cn.gy.test.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+	@RequestMapping(value = "/verifycode", method = RequestMethod.GET, produces = "image/jpg")
+	@ResponseBody
+	public byte[] getVerificationCode(HttpServletRequest request, HttpServletResponse response) {
+		
+		return null;
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@ResponseBody
+	public String login(HttpServletRequest request) {
+		
+		return "";
+	}
+}
---
+++ springbase/web/src/main/java/cn/gy/test/web/controller/WebController.java
@@ -0,0 +1,18 @@
+package cn.gy.test.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/web")
+public class WebController {
+
+	@RequestMapping(value = "/book", method = RequestMethod.GET)
+	public ResponseEntity<String> getBook() {
+		ResponseEntity<String> res = new ResponseEntity<String>("", HttpStatus.OK);
+		return res;
+	}
+}
